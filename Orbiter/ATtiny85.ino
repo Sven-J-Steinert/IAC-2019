41284095f171a9ca3bd06d34b64ea081560be4f4
Sven Steinert <46046233+unconsciou5@users.noreply.github.com>
2019-10-06 02:21:20 +0200

    Update ATtiny85.ino

--- Orbiter/ATtiny85.ino
+++ Orbiter/ATtiny85.ino
@@ -1,36 +1,46 @@
 #include "TinyWireS.h"
 
 #define SLAVE_ADDR 0x50
-int i=0;
-byte bb[4] ;
 
-void intToByte ( int i){
- bb[0]= (i >> 24) & 0xFF;
- bb[1]= (i >> 16) & 0xFF;
- bb[2]= (i >> 8) & 0xFF;
- bb[3]= i  & 0xFF;
+int sol=4,bat=2;
+byte bb[8] ;
+
+void intToByte (){
+ bb[0]= (bat >> 24) & 0xFF;
+ bb[1]= (bat >> 16) & 0xFF;
+ bb[2]= (bat >> 8) & 0xFF;
+ bb[3]= bat  & 0xFF;
+ bb[4]= (sol >> 24) & 0xFF;
+ bb[5]= (sol >> 16) & 0xFF;
+ bb[6]= (sol >> 8) & 0xFF;
+ bb[7]= sol  & 0xFF;
 }
-
 
 void setup()
 {
-  delay(5000);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
   TinyWireS.begin(SLAVE_ADDR);
-  delay(1000);
   TinyWireS.onRequest(requestEvent);
 }
 
 
 void loop()
 {
+  bat=analogRead(A3);
+  sol=analogRead(A2);
+  intToByte();
+  delay(100);
 }
 
 void requestEvent()
 {
-  i=analogRead(A3);
-  intToByte(i);
   TinyWireS.send(bb[0]);
   TinyWireS.send(bb[1]);
   TinyWireS.send(bb[2]);
   TinyWireS.send(bb[3]);
+  TinyWireS.send(bb[4]);
+  TinyWireS.send(bb[5]);
+  TinyWireS.send(bb[6]);
+  TinyWireS.send(bb[7]);
 }
