ea07b938fd1d0d131899995ea5969b5e7f4faa5a
Sven Steinert <46046233+unconsciou5@users.noreply.github.com>
2019-10-13 22:45:40 +0200

    Update Orbiter.ino

--- Orbiter/Orbiter.ino
+++ Orbiter/Orbiter.ino
@@ -9,29 +9,27 @@
 VL53L0X sensorR;
 VL53L0X sensorL;
 
-#define led1 4
+#define led1 15
 #define led2 13
 #define in2 12
-#define in4 15
+#define in4 4
 
 const int freq = 50000;
 const int M1FChannel = 0, M2FChannel = 1;
 const int resolution = 8;
 
-int r = 140, i =0, a=70; 
 byte pwmI, pwmA ;
-int distR, distL, dd=5, distT=0, calibR = 0,calibL = 0, offsetR = 4300, offsetL = 6000 ;
+int distR, distL, dd=5, calibR = 0,calibL = 0, offsetR = 43, offsetL = 60;
 int bat,sol, imgcount=0, psize;
 byte bb[8],bc[8], metab[6] ;
-byte tcmode=0, tctype, tctime, tcmode2=0, tctype2, tctime2;
-bool lowOrbit=false;
+byte tcmode=0, tctype=0, tctime=0;
+bool lowOrbit=true, picavail=false, insight=false;
 
-
-unsigned long currenttime,targettime,targettime2;
+unsigned long currenttime,targettime,lasttime=0;
+unsigned long check_wifi = 30000;
 
 WiFiUDP udp;
-char incomingPacket[24];  // buffer for incoming packets
-char controll = '1';       // data variable
+char incomingPacket[7];  // buffer for incoming packets
 
 IPAddress ServerIP(192,168,4,1);
 
@@ -109,32 +107,29 @@ sol |= (uint32_t) bb[7];
 TaskHandle_t Task1;
 
 void setup(void) {
-  
-    Serial.begin(115200);
-  
-  // Connect to WiFi network
+
+  pinMode(led1, OUTPUT); 
+  pinMode(led2, OUTPUT); 
+  pinMode(0, OUTPUT);
+  pinMode(16, OUTPUT);
+
+
+  WiFi.disconnect(true);
+  delay(500);
+  //register event handler
+  WiFi.mode(WIFI_STA);
+  delay(1000);
+  //Initiate connection
   WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
+ 
+  
   /*use mdns for host name resolution*/
   if (!MDNS.begin(host)) { //http://esp32.local
-    Serial.println("Error setting up MDNS responder!");
     while (1) {
       delay(1000);
     }
   }
-  Serial.println("mDNS responder started");
+  
   /*return index page which is stored in serverIndex */
   server.on("/", HTTP_GET, []() {
     server.sendHeader("Connection", "close");
@@ -148,26 +143,19 @@ void setup(void) {
   }, []() {
     HTTPUpload& upload = server.upload();
     if (upload.status == UPLOAD_FILE_START) {
-      Serial.printf("Update: %s\n", upload.filename.c_str());
       if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
-        Update.printError(Serial);
       }
     } else if (upload.status == UPLOAD_FILE_WRITE) {
       /* flashing firmware to ESP*/
       if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-        Update.printError(Serial);
       }
     } else if (upload.status == UPLOAD_FILE_END) {
       if (Update.end(true)) { //true to set the size to the current progress
-        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
       } else {
-        Update.printError(Serial);
       }
     }
   });
   server.begin();
-  
-  udp.begin(2000);
   
   ledcSetup(M1FChannel, freq, resolution);
   ledcSetup(M2FChannel, freq, resolution);
@@ -177,45 +165,90 @@ void setup(void) {
   
   ledcWrite(M1FChannel, 0);
   ledcWrite(M2FChannel, 0);
-
-  r = 140;
-  calcPWM();
-  //calcOffset();
-  
-  pinMode(led1, OUTPUT); 
-  pinMode(led2, OUTPUT); 
-  pinMode(0, OUTPUT);
-  pinMode(16, OUTPUT);
   
   digitalWrite(0, LOW);
   digitalWrite(16, LOW);
-  
+
+  //initialize 2 Distance-Sensors
   Wire.begin(2,14);
-  
   digitalWrite(16, HIGH);
   delay(150);
   sensorR.init(true);
   delay(100);
   sensorR.setMeasurementTimingBudget(200000);
   sensorR.setAddress((uint8_t)25);
-  //sensor.init();
-  //sensor.setTimeout(500);
-
   digitalWrite(0, HIGH);
   delay(150);
   sensorL.init(true);
   delay(100);
   sensorL.setMeasurementTimingBudget(200000);
 
+  udp.begin(WiFi.localIP(),2000);
+
+//calibration
+digitalWrite(led2, HIGH);
+calibR=0;
+calibL=0;
+ for (int i = 0; i < 10; i++) {
+    calibR=calibR+sensorR.readRangeSingleMillimeters();
+    delay(200);
+    calibL=calibL+sensorL.readRangeSingleMillimeters();
+    delay(200);
+  }
+  offsetR = (calibR/10)-20;
+  offsetL = (calibL/10)-20;
+
+if (offsetR>200 || offsetL>200){
+  calibR=0;
+  calibL=0;
+  for (int i = 0; i < 10; i++) {
+    calibR=calibR+sensorR.readRangeSingleMillimeters();
+    delay(200);
+    calibL=calibL+sensorL.readRangeSingleMillimeters();
+    delay(200);
+  }
+  offsetR = (calibR/10)-15;
+  offsetL = (calibL/10)-15;
+}
+  
+  byte oR[4],oL[4];
+ oR[0]= (offsetR >> 24) & 0xFF;
+ oR[1]= (offsetR >> 16) & 0xFF;
+ oR[2]= (offsetR >> 8) & 0xFF;
+ oR[3]= offsetR  & 0xFF;
+ oL[0]= (offsetL >> 24) & 0xFF;
+ oL[1]= (offsetL >> 16) & 0xFF;
+ oL[2]= (offsetL >> 8) & 0xFF;
+ oL[3]= offsetL  & 0xFF;
+ 
+ udp.beginPacket(ServerIP, 2005);
+ udp.write(oR[1]);
+ udp.write(oR[2]);
+ udp.write(oR[2]);
+ udp.write(oR[3]);
+ udp.endPacket();
+ udp.flush();
+ delay(100);
+ udp.beginPacket(ServerIP, 2005);
+ udp.write(oL[1]);
+ udp.write(oL[2]);
+ udp.write(oL[2]);
+ udp.write(oL[3]);
+ udp.endPacket();
+ udp.flush();
+ delay(100);
+digitalWrite(led2, LOW);
+delay(2000);
+
     digitalWrite(led1, HIGH);
     digitalWrite(led2, HIGH);
 
-
+// Go to Zero , Updatable
 while (dd>3 || dd<-3) {
    server.handleClient();
-    distR = sensorR.readRangeSingleMillimeters()-(offsetR/100);
+    distR = sensorR.readRangeSingleMillimeters()-offsetR;
     delay(200);
-    distL = sensorL.readRangeSingleMillimeters()-(offsetL/100);
+    distL = sensorL.readRangeSingleMillimeters()-offsetL;
     delay(200);
     dd = distR -distL;
     if ((dd<3)&&(dd>-3)){
@@ -237,7 +270,7 @@ while (dd>3 || dd<-3) {
   digitalWrite(led2, LOW);
   delay(500);
 
- /*
+
   xTaskCreatePinnedToCore(
                     Task1code,   // Task function. 
                     "Task1",     // name of task. 
@@ -245,105 +278,80 @@ while (dd>3 || dd<-3) {
                     NULL,        // parameter of the task 
                     1,           // priority of the task 
                     &Task1,      // Task handle to keep track of created task 
-                    0);          // pin task to core 0  
-*/
+                    1);          // pin task to core 1  
+
                    
 }
 
 void loop(void) {
- readUDP();
-Wire.requestFrom(0x50, 8); // request 1 byte from slave device address 4
-int u=0;
-while(Wire.available()) // slave may send less than requested
- {
- bb[u]= Wire.read();
- Serial.println(bb[u]);
- u++;
- }
+    
+    Wire.requestFrom(0x50, 8); // request 1 byte from slave device address 4
+    byte u=0;
+    while(Wire.available()) // slave may send less than requested
+      {
+        bb[u]= Wire.read();
+        u++;
+      }
  
- ByteToint();
- bat=map(bat,0,1023,0,6600);
- sol=map(sol,0,1023,0,6600);
- intToByte();
-
+    ByteToint();
+    bat=map(bat,0,1023,0,6600);
+    sol=map(sol,0,1023,0,6600);
+    intToByte();
+    delay(10);
    
-    distR = sensorR.readRangeSingleMillimeters()-(offsetR/100);
-    delay(200);
-    distL = sensorL.readRangeSingleMillimeters()-(offsetL/100);
-    delay(200);
-    dd = distR -distL;
-         
-    calcPWM();
+    distR = sensorR.readRangeSingleMillimeters()-offsetR;
+    delay(220);
+    distL = sensorL.readRangeSingleMillimeters()-offsetL;
+    delay(220);
+    dd = distR-distL;
+    
+    pwmI = 70;
+    pwmA = 135; 
+    //pwmA = 130;  
     
     if (dd>5){
       int val = pwmA +(4*dd);
       pwmA = round( min(val, 255));
-    }
-    if (dd<-5){
+    }else if (dd<-5){
       int val = pwmI +(-4*dd);
       pwmI = round( min(val, 255));
     }
-   /*
-if ((dd<5)&&(dd>-5)){
-    int distT;
-    distT = round((distR+distL)/2);
-    ddT = distT-a;
-
-    if (distT > 80){
-      pwmA=max(pwmA+20, 0);
-    }
-    if (distT < 50){
-      pwmI=max(pwmI+20, 0);
-    }*/
     
-
     ledcWrite(M1FChannel, pwmA);
     ledcWrite(M2FChannel, pwmI);
 
+
+// run TC 
 currenttime = millis();
 if (currenttime > targettime){
-     digitalWrite(led1, HIGH);
-    delay(200);
-    digitalWrite(led1, LOW);
   if (tcmode == 1){
+    digitalWrite(led1, HIGH);
       if (tctype == 1){
         //move to low Orbit
-             lowOrbit = true;
              ledcWrite(M1FChannel, 255);
-             ledcWrite(M2FChannel, 55);
-             delay(4000);
-    
-        while (dd>5 || dd<-5) {
-        distR = sensorR.readRangeSingleMillimeters()-(offsetR/100);
-        delay(200);
-        distL = sensorL.readRangeSingleMillimeters()-(offsetL/100);
-        delay(200);
-        dd = distR -distL;
+             ledcWrite(M2FChannel, 80);
+             delay(1500);
         ledcWrite(M1FChannel, 100);
-        ledcWrite(M2FChannel, 95);
-      }}
+        ledcWrite(M2FChannel, 100);
+        delay(1500);
+        digitalWrite(led1, LOW);
+      }
     if (tctype == 2){
         //move to high Orbit
-            lowOrbit = false;
-            ledcWrite(M1FChannel, 150);
-            ledcWrite(M2FChannel, 150);
-            delay(3500);
-
-            while (dd>5 || dd<-5) {
-            distR = sensorR.readRangeSingleMillimeters()-(offsetR/100);
-            delay(200);
-            distL = sensorL.readRangeSingleMillimeters()-(offsetL/100);
-            delay(200);
-            dd = distR -distL;
-            ledcWrite(M1FChannel, 250);
-            ledcWrite(M2FChannel, 50);
-            }
-    }}
+            ledcWrite(M1FChannel, 200);
+            ledcWrite(M2FChannel, 180);
+            delay(3000);
+            digitalWrite(led1, LOW);
+    }
+    tcmode = 0;
+    tctype = 0;
+    tctime = 0;
+  }
  if (tcmode == 2){
     digitalWrite(led1, HIGH);
-    delay(20);
+    delay(100);
     digitalWrite(led1, LOW);
-    delay(20);
+    delay(50);
     digitalWrite(led1, HIGH);
       imgcount++;
       metab[0]=imgcount;
@@ -367,128 +375,43 @@ if (currenttime > targettime){
        metab[3]= (psize >> 16) & 0xFF;
        metab[4]= (psize >> 8) & 0xFF;
        metab[5]= psize  & 0xFF;
-       
-        // send meta
-       udp.beginPacket(ServerIP, 2002);
-       udp.write(metab[0]);
-       udp.write(metab[1]);
-       udp.write(metab[2]);
-       udp.write(metab[3]);
-       udp.write(metab[4]);
-       udp.write(metab[5]);
-       udp.endPacket();
-       udp.flush();
+
+       picavail=true;
 
        digitalWrite(led1, LOW);
+    tcmode = 0;
+    tctype = 0;
+    tctime = 0;
     }
-    resetTC();
-}
-      
-
     
 
-/*
- if (currenttime > targettime2){
-    if (tcmode2 == 1){
-      if (tctype2 == 1){
-        //move to low Orbit
-      }
-      if (tctype2 == 2){
-        //move to high Orbit
-      }
-    }
-    if (tcmode2 == 2){
-      if (tctype2 == 1){
-        // photo res = 640x480
-      }
-      if (tctype2 == 2){
-        // photo res = 1024x768
-      }
-      if (tctype2 == 3){
-        // photo res = 1600x1200
-      }
-    }
-    }
-*/
+}
+    
 }
 
 void Task1code( void * pvParameters ){
 
   for(;;){
- server.handleClient();
- 
-udp.beginPacket(ServerIP, 2000);
- udp.write(bc[0]);
- udp.write(bc[1]);
- udp.write(bc[2]);
- udp.write(bc[3]);
- udp.endPacket();
- udp.flush();
-delay(200);
-udp.beginPacket(ServerIP, 2001);
- udp.write(bc[4]);
- udp.write(bc[5]);
- udp.write(bc[6]);
- udp.write(bc[7]);
- udp.endPacket();
- udp.flush();
- delay(800);
-}
-  
-}
 
-
-void calcPWM()
-{
-  if (lowOrbit == true){
-    pwmI = 85;
-    pwmA = 130;  
-  }else{
-    pwmI = 100;
-    pwmA = 150;
+    // if wifi is down, try reconnecting every 5 seconds
+  if ((WiFi.status() != WL_CONNECTED) && (millis() > check_wifi)) {
+    WiFi.disconnect(true);
+    delay(50);
+    WiFi.mode(WIFI_STA);
+    delay(100);
+    WiFi.begin(ssid, password);
+    udp.begin(WiFi.localIP(),2000);
+    check_wifi = millis() + 5000;
   }
-
-}
-
-void calcOffset()
-{
-  a=r-70;
-  if (a>=90){
-    offsetR = 2800;
-    offsetL = 3800;
- }
-  if ((a<90)&(a>=65)){
-    offsetR = 3300;
-    offsetL = 4400;
-  } 
-  if ((a<65)&(a>=35)){
-    offsetR = 4000;
-    offsetL = 5300;
-  } 
-  if (a<35){
-    offsetR = 5200;
-    offsetL = 5500;
-  } 
-}
-
-
-void readUDP()
-{
-  int packetSize = udp.parsePacket();
-  if (packetSize)
-  { 
-    int len = udp.read(incomingPacket, 24);
-   /* if (len > 0)
-    {
-      incomingPacket[len] = 0;
-    }
-    */
-    
+   
+    int packetSize = udp.parsePacket();
+  if (packetSize){ 
+    int len = udp.read(incomingPacket, 7);
     digitalWrite(led1, HIGH);
     delay(100);
     digitalWrite(led1, LOW);
     
-if (tcmode==0){
+   if (tcmode==0){
  
     tcmode = incomingPacket[0] - 0x30;
     tctype = incomingPacket[2] - 0x30;
@@ -499,36 +422,62 @@ if (tcmode==0){
     String myString = x1+x2+x3; 
     tctime = myString.toInt();
     targettime = millis() + tctime*1000;
-}else{
-    tcmode2 = incomingPacket[3];
-    tctype2 = incomingPacket[7];
-    tctime2 = incomingPacket[11];
-    targettime2 = millis()+ tctime2*1000;
-} 
 
- udp.beginPacket(ServerIP, 2005);
- for (int v=0;v<24;v++){
-   udp.write(incomingPacket[v]);
- }
+    udp.beginPacket(ServerIP, 2006);
+    udp.write(tcmode);
+    udp.write(tctype);
+    udp.write(tctime);
+    udp.endPacket();
+    udp.flush();
+
+    incomingPacket[0]=0;
+    incomingPacket[1]=0;
+    incomingPacket[2]=0;
+    incomingPacket[3]=0;
+    incomingPacket[4]=0;
+    incomingPacket[5]=0;
+    incomingPacket[6]=0;
+    incomingPacket[7]=0;
+    }
+
+    }
+    
+if (currenttime > lasttime+2000){
+// Send TM Bat-Voltage Sol-Voltage
+ udp.beginPacket(ServerIP, 2000);
+ udp.write(bc[0]);
+ udp.write(bc[1]);
+ udp.write(bc[2]);
+ udp.write(bc[3]);
  udp.endPacket();
  udp.flush();
-delay(100);
- udp.beginPacket(ServerIP, 2005);
- udp.write(tcmode);
- udp.write(tctype);
- udp.write(tctime);
+ delay(100);
+udp.beginPacket(ServerIP, 2001);
+ udp.write(bc[4]);
+ udp.write(bc[5]);
+ udp.write(bc[6]);
+ udp.write(bc[7]);
  udp.endPacket();
  udp.flush();
-
- for (int v=0;v<24;v++){
-   incomingPacket[v]=0;
- }
-   }
+ lasttime=millis();
 }
 
-void resetTC()
-{
-    tcmode = 0;
-    tctype = 0;
-    tctime = 0;
+
+if (picavail){
+  // send Photo meta
+       udp.beginPacket(ServerIP, 2002);
+       udp.write(metab[0]);
+       udp.write(metab[1]);
+       udp.write(metab[2]);
+       udp.write(metab[3]);
+       udp.write(metab[4]);
+       udp.write(metab[5]);
+       udp.endPacket();
+       udp.flush();
+       picavail=false;
 }
+   
+  } 
+}
+
+
